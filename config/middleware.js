b98eaefb697b249bee4ae43e02d6f6016fcf7476
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-19 09:05:23 -0700

    Working on Register & Login

---
+++ config/middleware.js
@@ -0,0 +1,56 @@
+const jwt = require("jsonwebtoken");
+
+const jwtKey = require("../secrets/keys").jwtKey;
+
+// quickly see what this file exports
+module.exports = {
+  authenticate,
+  generateToken
+};
+
+// implementation details
+function authenticate(req, res, next) {
+  const token = req.get("Authorization");
+  console.log(token);
+
+  if (token) {
+    jwt.verify(token, jwtKey, (err, decoded) => {
+      if (err) return res.status(401).json(err);
+
+      req.decoded = decoded;
+
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      error: "No token provided, must be set on the Authorization Header"
+    });
+  }
+}
+
+function generateToken(user) {
+    const jwtPayload = {
+      ...user,
+      extra: "extra info",
+     subject: user.id,
+    username: user.username,
+        jokes: ['Dad'],
+    };
+    const jwtOptions = {
+      expiresIn: "1d"
+    };
+    return jwt.sign(jwtPayload, jwtKey, jwtOptions);
+  }
+
+// function generateToken(user) {
+//   const payload = {
+//     subject: user.id,
+//     username: user.username,
+//     jokes: ['Dad'],
+//   }
+
+//   const options = {
+//     expiresIn: '1d'
+//   }
+//   return jwt.sign(payload, secrets.jwtSecret, jwtKey, options)
+// }
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
 
-const { authenticate } = require('../auth/authenticate');
+const db = require("../database/dbConfig.js");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const { authenticate, generateToken } = require('../config/middleware.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,11 +14,50 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  db('users')
+  .insert(user)
+    .then(saved => {
+              // const saved = ids[0];
+      res.status(201).json(
+        { 
+        saved
+        // userId: id 
+      }
+      );
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
+
 
 function login(req, res) {
   // implement user login
+  db("users")
+//   Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        // the server needs to return the token to the client
+        // this doesn't happen automatically like it happens with cookies
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
+
 
 function getJokes(req, res) {
   const requestOptions = {
