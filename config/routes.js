ed619432e5b9cb6805491227a0beff94b4d279fe
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-19 10:11:20 -0700

    Login is working

--- config/routes.js
+++ config/routes.js
@@ -40,7 +40,6 @@ function login(req, res) {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
-
         // the server needs to return the token to the client
         // this doesn't happen automatically like it happens with cookies
         res.status(200).json({
